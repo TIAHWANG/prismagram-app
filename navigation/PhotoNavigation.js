d6b66fb7e910280f0edc07e8ae76d27e904c780a
Tia Hwang <tiajshwang@gmail.com>
2020-06-01 15:18:28 +0900

    PhotoNavigation

---
+++ navigation/PhotoNavigation.js
@@ -0,0 +1,28 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import SelectPhoto from "../screens/Photo/SelectPhoto";
+import TakePhoto from "../screens/Photo/TakePhoto";
+import UploadPhoto from "../screens/Photo/UploadPhoto";
+
+const PhotoTabs = createMaterialTopTabNavigator();
+const PhotoNavigation = createStackNavigator();
+
+const PhotoTab = () => {
+    return (
+        <PhotoTabs.Navigator tabBarPosition="bottom">
+            <PhotoTabs.Screen name="SelectPhoto" component={SelectPhoto} />
+            <PhotoTabs.Screen name="TakePhoto" component={TakePhoto} />
+        </PhotoTabs.Navigator>
+    );
+};
+
+export default () => {
+    return (
+        <PhotoNavigation.Navigator headerMode="none">
+            <PhotoNavigation.Screen name="PhotoTab" component={PhotoTab} />
+            <PhotoNavigation.Screen name="UploadPhoto" component={UploadPhoto} />
+        </PhotoNavigation.Navigator>
+    );
+};
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,7 +1,6 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
@@ -12,28 +11,26 @@ const TabNavigation = createBottomTabNavigator();
 
 export default () => {
     return (
-        <NavigationContainer>
-            <TabNavigation.Navigator
-                initialRouteName="Home"
-                tabBarOptions={{
-                    activeTintColor: "#e91e63",
-                }}
-            >
-                <TabNavigation.Screen name="Home" component={Home} />
-                <TabNavigation.Screen name="Search" component={Search} />
-                <TabNavigation.Screen
-                    name="Add"
-                    component={View}
-                    listeners={{
-                        tabPress: (e) => {
-                            e.preventDefault();
-                            console.log("Add");
-                        },
-                    }}
-                />
-                <TabNavigation.Screen name="Notifications" component={Notifications} />
-                <TabNavigation.Screen name="Profile" component={Profile} />
-            </TabNavigation.Navigator>
-        </NavigationContainer>
+        <TabNavigation.Navigator
+            initialRouteName="Home"
+            tabBarOptions={{
+                activeTintColor: "#e91e63",
+            }}
+        >
+            <TabNavigation.Screen name="Home" component={Home} />
+            <TabNavigation.Screen name="Search" component={Search} />
+            <TabNavigation.Screen
+                name="Add"
+                component={View}
+                listeners={({ navigation }) => ({
+                    tabPress: (e) => {
+                        e.preventDefault();
+                        navigation.navigate("PhotoNavigation");
+                    },
+                })}
+            />
+            <TabNavigation.Screen name="Notifications" component={Notifications} />
+            <TabNavigation.Screen name="Profile" component={Profile} />
+        </TabNavigation.Navigator>
     );
 };
