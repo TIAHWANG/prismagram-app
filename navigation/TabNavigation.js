d4d0876610931158a9f43f3b7c8380bb91676a47
Tia Hwang <tiajshwang@gmail.com>
2020-06-03 00:06:45 +0900

    Tab Icons

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,13 +1,15 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Platform } from "react-native";
 import stackFactory from "./stackFactory";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Profile from "../screens/Profile";
 import MessageLink from "../components/MessageLink";
+import NavIcon from "../components/NavIcon";
 
 const TabNavigation = createBottomTabNavigator();
 
@@ -16,14 +18,13 @@ export default () => {
         <TabNavigation.Navigator
             initialRouteName="Home"
             tabBarOptions={{
-                activeTintColor: "#e91e63",
+                showLabel: false,
             }}
         >
             <TabNavigation.Screen
                 name="Home"
                 component={stackFactory}
                 initialParams={{
-                    name: "Home",
                     initialRoute: Home,
                     customConfig: {
                         title: "Home",
@@ -31,14 +32,21 @@ export default () => {
                             height: 80,
                         },
                         headerRight: () => <MessageLink />,
+                        headerTitle: (
+                            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                                <Image style={{ height: 30 }} resizeMode="contain" source={require("../assets/logo.png")} />
+                            </View>
+                        ),
                     },
+                }}
+                options={{
+                    tabBarIcon: ({ focused }) => <NavIcon focused={focused} name={Platform.OS === "ios" ? "ios-home" : "md-home"} />,
                 }}
             />
             <TabNavigation.Screen
                 name="Search"
                 component={stackFactory}
                 initialParams={{
-                    name: "Home",
                     initialRoute: Search,
                     customConfig: {
                         title: "Search",
@@ -46,6 +54,9 @@ export default () => {
                             height: 80,
                         },
                     },
+                }}
+                options={{
+                    tabBarIcon: ({ focused }) => <NavIcon focused={focused} name={Platform.OS === "ios" ? "ios-search" : "md-search"} />,
                 }}
             />
             <TabNavigation.Screen
@@ -57,6 +68,9 @@ export default () => {
                         navigation.navigate("PhotoNavigation");
                     },
                 })}
+                options={{
+                    tabBarIcon: ({ focused }) => <NavIcon focused={focused} size={28} name={Platform.OS === "ios" ? "ios-add" : "md-add"} />,
+                }}
             />
             <TabNavigation.Screen
                 name="Notifications"
@@ -70,6 +84,14 @@ export default () => {
                         },
                     },
                 }}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <NavIcon
+                            focused={focused}
+                            name={Platform.OS === "ios" ? (focused ? "ios-heart" : "ios-heart-empty") : focused ? "md-heart" : "md-heart-empty"}
+                        />
+                    ),
+                }}
             />
             <TabNavigation.Screen
                 name="Profile"
@@ -82,6 +104,9 @@ export default () => {
                             height: 80,
                         },
                     },
+                }}
+                options={{
+                    tabBarIcon: ({ focused }) => <NavIcon focused={focused} name={Platform.OS === "ios" ? "ios-person" : "md-person"} />,
                 }}
             />
         </TabNavigation.Navigator>
--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import Loader from "../components/Loader";
 
 const View = styled.View`
     justify-content: center;
     align-items: center;
     flex: 1;
+    background-color: white;
 `;
 
 const Text = styled.Text``;
 
 export default () => (
     <View>
-        <Text>Home</Text>
+        <Loader />
+        {/* <Text>Home</Text> */}
     </View>
 );
